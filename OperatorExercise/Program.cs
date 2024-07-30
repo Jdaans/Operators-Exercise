7a1ce9b218ea30a2ab4f45a53e43d68d17525be5
Josh Daniels <jdaans95@gmail.com>
2024-07-30 14:32:09 -0700

    Operators Exercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 40;
+            int b = 50;
+
+            int sumAdd = a + b;
+            Console.WriteLine(sumAdd);
+
+            int sumSubtract = a - b;
+            Console.WriteLine(sumSubtract);
+
+            int sumMultiply = a * b;
+            Console.WriteLine(sumMultiply);
+
+
+            int x = 24;
+            int y = 12;
+
+            int quotient = x / y;
+            
+            int remainder = x % y;
+
+            Console.WriteLine($"{x}/{y} is {quotient} remainder {remainder}");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            AreaOfCircle(radius);
+
+           
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+           
+
+           
+        return Math.PI * Math.Pow(radius, 2);
+
+
+
         }
     }
 }
